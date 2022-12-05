bb30847514fbf9be420dbf325e46a0d113968d84
Mikael Grimstad <mikael.grimstad@gmail.com>
2022-12-05 10:07:53 +0100

    Day 5

---
+++ 2022/5-2/main.go
@@ -0,0 +1,110 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"regexp"
+	"strconv"
+	"strings"
+)
+
+func reverseArray(arr []string) []string {
+	for i, j := 0, len(arr)-1; i < j; i, j = i+1, j-1 {
+		arr[i], arr[j] = arr[j], arr[i]
+	}
+	return arr
+}
+
+type StackString struct {
+	s []string
+}
+
+func (s *StackString) Push(value string) {
+	s.s = append(s.s, value)
+}
+
+func (s *StackString) Pop() string {
+	length := len(s.s)
+	res := s.s[length-1]
+	s.s = s.s[:length-1]
+	return res
+}
+func (s *StackString) Top() string {
+	length := len(s.s)
+	res := s.s[length-1]
+	return res
+}
+func (s *StackString) IsEmpty() bool {
+	length := len(s.s)
+	return length == 0
+}
+
+func main() {
+	content, _ := ioutil.ReadFile("input.txt")
+	lines := strings.Split(string(content), "\n")
+
+	stacks := map[int]StackString{}
+
+	stackLines := []string{}
+	orderLines := []string{}
+	for _, line := range lines {
+		line = strings.Replace(line, "\r", "", -1)
+		if strings.Contains(line, "[") {
+			stackLines = append(stackLines, line)
+		}
+		if strings.Contains(line, "move") {
+			orderLines = append(orderLines, line)
+		}
+	}
+	for _, line := range reverseArray(stackLines) {
+		i := 1
+		for len(line) >= 3 {
+			if _, ok := stacks[i]; !ok {
+				stacks[i] = StackString{}
+			}
+			stack := stacks[i]
+
+			sliceRange := 4
+			if len(line) < 4 {
+				sliceRange = 3
+			}
+			sLine := line[:sliceRange]
+			line = line[sliceRange:]
+
+			val := string(sLine[1])
+			trimmed := strings.TrimSpace(val)
+			if len(trimmed) > 0 {
+				stack.Push(trimmed)
+				stacks[i] = stack
+			}
+			i++
+		}
+	}
+	for _, line := range orderLines {
+		r, _ := regexp.Compile("[0-9]+")
+		orders := r.FindAllString(line, -1)
+		from, _ := strconv.Atoi(orders[1])
+		to, _ := strconv.Atoi(orders[2])
+		iterations, _ := strconv.Atoi(orders[0])
+		crates := []string{}
+		for i := 0; i < iterations; i++ {
+			sourceStack := stacks[from]
+			crate := sourceStack.Pop()
+			stacks[from] = sourceStack
+			crates = append(crates, crate)
+		}
+		targetStack := stacks[to]
+		for _, crate := range reverseArray(crates) {
+			targetStack.Push(crate)
+		}
+		stacks[to] = targetStack
+	}
+	for i := 1; i <= len(stacks); i++ {
+		stack := stacks[i]
+		if stack.IsEmpty() {
+			fmt.Print("-")
+		} else {
+			fmt.Print(stack.Top())
+		}
+	}
+}
