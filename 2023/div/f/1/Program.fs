7a4db31ccf6e64fca230ed98381998907acb52e7
Mikael Grimstad <mikael.grimstad@gmail.com>
2023-12-05 12:22:32 +0100

    2023 1

---
+++ 2023/div/f/1/Program.fs
@@ -0,0 +1,18 @@
+﻿open System
+open System.IO
+
+let lines = File.ReadAllLines(__SOURCE_DIRECTORY__ + "\\input.txt")
+
+let list = Seq.toList lines
+
+let mutable result: int = 0;
+
+for item in list do
+    let charSeq = item |> Seq.toList
+    let firstNumberOpt = Seq.find Char.IsDigit charSeq
+    let lastNumberOpt = Seq.findBack Char.IsDigit charSeq
+
+    let number = int(string firstNumberOpt + string lastNumberOpt)
+    result <- result + number
+
+printfn "Sum %d" result
