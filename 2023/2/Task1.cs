f8293ff6089d5677d879e0311b89b843d580f087
Mikael Johansen Grimstad <mikael.johansen.grimstad@kongsbergdigital.com>
2023-12-06 15:37:30 +0100

    2

--- 2023/2/Task1.cs
+++ 2023/2/Task1.cs
@@ -1,16 +1,62 @@
-public class Task1{
+public class Task1
+{
     private string[] _input;
-    public Task1(string[] input){
+    public Task1(string[] input)
+    {
         _input = input;
     }
-    public void Solve(){
-        int result = 0;
+    private List<List<List<(int count, string color)>>> GetGames()
+    {
+        var games = new List<List<List<(int count, string color)>>>();
 
-        foreach (var line in _input){
-            var isInt = (char item) => int.TryParse(item.ToString(), out var _);
-            var firstN = line.First(isInt).ToString();
-            var lastN = line.Last(isInt).ToString();
-            result+= int.Parse(firstN+lastN);
+        foreach (var line in _input)
+        {
+            var retGame = new List<List<(int count, string color)>>();
+            var game = line.Split(':')[1];
+            var sets = game.Split(";");
+            foreach (var set in sets)
+            {
+                var retSet = new List<(int count, string color)>();
+                var cubes = set.Split(",");
+                foreach (var cube in cubes)
+                {
+                    var split = cube.Trim().Split(" ");
+                    retSet.Add((int.Parse(split[0]), split[1]));
+                }
+                retGame.Add(retSet);
+            }
+            games.Add(retGame);
+        }
+        return games;
+    }
+    private bool IsValidSet(List<(int count, string color)> set)
+    {
+        var maxC = new Dictionary<string, int>()
+            {
+                { "red" , 12},
+                { "green" , 13},
+                { "blue" , 14}
+            };
+        foreach (var max in maxC)
+        {
+            if (set.Where(item => item.color == max.Key).Sum(item => item.count) > max.Value)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+    public void Solve()
+    {
+        var games = GetGames();
+
+        int result = 0;
+        foreach (var game in games)
+        {
+            if (game.All(IsValidSet))
+            {
+                result += games.IndexOf(game) + 1;
+            }
         }
         Console.WriteLine(result);
     }
