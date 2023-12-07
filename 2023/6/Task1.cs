8ce010a69462b78989392d2cac8921b6438c9a11
Mikael Grimstad <mikael.grimstad@gmail.com>
2023-12-07 17:47:11 +0100

    6

---
+++ 2023/6/Task1.cs
@@ -0,0 +1,45 @@
+using System.Text.RegularExpressions;
+
+public class Task1
+{
+    private string[] _input;
+    public Task1(string[] input)
+    {
+        _input = input;
+    }
+    private List<(int MaxTime, int Distance)> GetRaces()
+    {
+        string trimAllDoubleSpaces(string input) => Regex.Replace(input, @"\s+", " ");
+        List<int> getInts(string input) => input.Split(":")[1].Trim().Split(" ").Select(item => int.Parse(item)).ToList();
+        var times = getInts(trimAllDoubleSpaces(_input[0]));
+        var distances = getInts(trimAllDoubleSpaces(_input[1]));
+        return times.Select(item => (item, distances[times.IndexOf(item)])).ToList();
+    }
+    private int? Simulate(int maxTime, int speed, int distance)
+    {
+        var timeRequired = distance / speed;
+        return (timeRequired<maxTime ? timeRequired : null);
+    }
+    public void Solve()
+    {
+        var races = GetRaces();
+
+        //var result = races.Select(race =>
+        //{
+        //    var possibleWins = 0;
+        //    for (var i = 1; i < race.MaxTime; i++)
+        //    {
+        //        var simulatedTime = Simulate(race.MaxTime - i, i, race.Distance);
+        //        if (simulatedTime.HasValue && simulatedTime.Value <= race.MaxTime)
+        //        {
+        //            possibleWins++;
+        //        }
+        //    }
+        //    return possibleWins;
+        //}).Aggregate((a, b) => a*b);
+
+        var result = races.Select(race => Enumerable.Range(1, race.MaxTime).Select(startTime => Simulate(race.MaxTime - startTime, startTime, race.Distance)).Where(raceTime => raceTime.HasValue).Count()).Aggregate((a, b) => a * b);
+
+        Console.WriteLine(result);
+    }
+}
