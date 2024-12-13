5e53ef4257f42b9916e036c533b092bffcc079ff
Mikael Grimstad <6338915+mikjgri@users.noreply.github.com>
2024-12-13 09:34:47 +0100

    2024-12

--- 2024/12/Task2.cs
+++ 2024/12/Task2.cs
@@ -9,7 +9,7 @@ public class Task2(string[] input)
         var grid = input.CreateGrid();
         var coordinates = GridTools.GenerateCoordinates(grid[0].Count, grid.Count);
         var squareDirections = GridTools.GetSquare4DirectionOffsets().ToList();
-        var dir8 = GridTools.Get8DirectionOffsets();
+        var diagonalDirections = GridTools.GetDiagonal4DirectionOffsets().ToList();
 
         var handledCoordinates = new List<(int x, int y)>();
 
@@ -20,72 +20,83 @@ public class Task2(string[] input)
             gardens.Add(GetGarden(coord, grid[coord.y][coord.x], new FlowerGarden()));
         }
 
-        foreach (var garden in gardens)
+        Console.WriteLine(gardens.Sum(garden =>
         {
             var corners = 0;
             foreach (var spot in garden.Region)
             {
-                var left = ((spot.x - 1, spot.y));
-                var containsLeft = garden.Region.Contains(left);
-                var right = ((spot.x + 1, spot.y));
-                var containsRight = garden.Region.Contains(right);
-                var up = ((spot.x, spot.y - 1));
-                var containsUp = garden.Region.Contains(up);
-                var down = ((spot.x, spot.y + 1));
-                var containsDown = garden.Region.Contains(down);
+                var containsLeft = garden.Region.Contains(((spot.x - 1, spot.y)));
+                var containsRight = garden.Region.Contains(((spot.x + 1, spot.y)));
+                var containsUp = garden.Region.Contains(((spot.x, spot.y - 1)));
+                var containsDown = garden.Region.Contains(((spot.x, spot.y + 1)));
+                var containsUpLeft = garden.Region.Contains(((spot.x - 1, spot.y - 1)));
+                var containsUpRight = garden.Region.Contains(((spot.x + 1, spot.y - 1)));
+                var containsDownLeft = garden.Region.Contains(((spot.x - 1, spot.y + 1)));
+                var containsDownRight = garden.Region.Contains(((spot.x + 1, spot.y + 1)));
 
                 var containsLeftAndRight = containsLeft && containsRight;
                 var containsUpAndDown = containsUp && containsDown;
 
-                if (containsLeftAndRight || containsUpAndDown) //not corner
+                //check outer
+                var squareNeighbours = squareDirections.Where(dir => garden.Region.Contains((spot.x + dir.xOff, spot.y + dir.yOff))).ToList();
+                if (containsLeftAndRight || containsUpAndDown)
                 {
-                    continue;
+                    //not outer corner
+                }
+                else if (squareNeighbours.Count == 0)
+                {
+                    corners += 4; //single region
+                }
+                else if (squareNeighbours.Count == 1)
+                {
+                    corners += 2; //one neighbour must be two outer corners
+                }
+                else if (squareNeighbours.Count == 2)
+                {
+                    corners++; //two neighbours must be one outer corner
                 }
 
-                var neighbours = squareDirections.Where(dir => garden.Region.Contains((spot.x + dir.xOff, spot.y + dir.yOff))).ToList();
-                if (neighbours.Count == 0)
+                //check inner
+                var diagonalNeighbours = diagonalDirections.Where(dir => garden.Region.Contains((spot.x + dir.xOff, spot.y + dir.yOff))).ToList();
+                if (diagonalNeighbours.Count == 4)
                 {
-                    corners += 4;
-                    continue;
+                    continue; //not inner corner
                 }
-                
-                if (a == 1)
+                if (containsUp && containsRight && !containsUpRight)
                 {
-                    corners += 2;
+                    corners++;
                 }
-                else
+                if (containsRight && containsDown && !containsDownRight)
                 {
-                    corners ++;
+                    corners++;
                 }
-                var b = 123;
+                if (containsDown && containsLeft && !containsDownLeft)
+                {
+                    corners++;
+                }
+                if (containsLeft && containsUp && !containsUpLeft)
+                {
+                    corners++;
+                }
+
             }
-            Console.WriteLine(corners);
-        }
+            return corners * garden.Region.Count;
+        }));
 
         FlowerGarden GetGarden((int x, int y) pos, char plantType, FlowerGarden garden)
         {
             if (garden.Region.Contains(pos)) return garden;
 
-            if (!GridTools.IsInGrid(pos, grid) || grid[pos.y][pos.x] != plantType)
-            {
-                garden.Perimeter.Add(pos);
-            }
-            else
+            if (GridTools.IsInGrid(pos, grid) && grid[pos.y][pos.x] == plantType)
             {
                 garden.Region.Add(pos);
-                squareDirections.ToList().ForEach(dir => GetGarden((pos.x + dir.xOff, pos.y + dir.yOff), plantType, new FlowerGarden() { Region = garden.Region, Perimeter = garden.Perimeter }));
+                squareDirections.ToList().ForEach(dir => GetGarden((pos.x + dir.xOff, pos.y + dir.yOff), plantType, new FlowerGarden() { Region = garden.Region }));
             }
             return garden;
         }
     }
     class FlowerGarden
     {
-        public List<(int x, int y)> Perimeter = [];
         public List<(int x, int y)> Region = [];
-    }
-    class Traverse
-    {
-        public (int x, int y) Fence;
-        public (int xOff, int yOff) Direction;
     }
 }
