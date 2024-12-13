a93814791442fbaac9dcc9720cccb6c465352e24
Mikael Grimstad <mikael.grimstad@gmail.com>
2024-12-13 07:45:31 +0100

    2024-12

--- 2024/12/Program.cs
+++ 2024/12/Program.cs
@@ -1,3 +1,3 @@
-﻿var text = File.ReadAllLines(Directory.GetCurrentDirectory() + "\\input.txt");
-new Task1(text).Solve();
-//new Task2(text).Solve();
+﻿var text = File.ReadAllLines(Directory.GetCurrentDirectory() + "\\demo.txt");
+//new Task1(text).Solve();
+new Task2(text).Solve();
