35a850adfbbd52971a2d1b065feaefe96d190259
Mikael Grimstad <mikael.grimstad@gmail.com>
2025-12-10 21:39:36 +0100

    2025-10 p1

---
+++ 2025/10/Task1.cs
@@ -0,0 +1,59 @@
+using CommonLib;
+using System.Text.RegularExpressions;
+
+public class Task1(string[] input) : BaseTask()
+{
+    protected override void Solve()
+    {
+        var machines = input.Select(line =>
+        {
+            var lightGoalState = Regex.Match(line, @"\[([.#]+)\]").Groups[1].Value.Select(c => c == '#').ToArray();
+            var buttons = Regex.Matches(line, @"\((\d+(?:\s*,\s*\d+)*)\)").Select(match =>
+            {
+                var split = match.Groups[1].Value.Split(",");
+                return new Button([.. split.Select(s => int.Parse(s))]);
+            }).ToArray();
+            return new Machine(lightGoalState, buttons);
+        }).ToList();
+
+        Console.WriteLine(machines.Sum(machine =>
+        {
+            int? bestFlipCount = null;
+            var dynamicFailOutValue = 1;
+            while (bestFlipCount == null)
+            {
+                FlipSwitches([.. Enumerable.Range(0, machine.LightGoalState.Length).Select(i => false)], 0);
+                dynamicFailOutValue++;
+            }
+            Console.WriteLine($"{machines.IndexOf(machine) + 1}/{machines.Count} - {bestFlipCount.Value}");
+            return bestFlipCount;
+
+            void FlipSwitches(bool[] state, int flipCount, Button? pressedButton = null)
+            {
+                if (pressedButton != null)
+                {
+                    foreach (var modifier in pressedButton.Modifiers)
+                    {
+                        state[modifier] = !state[modifier];
+                    }
+                    flipCount++;
+                }
+                if (flipCount > dynamicFailOutValue) return;
+                if (bestFlipCount != null && flipCount > bestFlipCount) return;
+                if (state.SequenceEqual(machine.LightGoalState))
+                {
+                    if (bestFlipCount == null || flipCount < bestFlipCount) bestFlipCount = flipCount;
+                    return;
+                }
+
+                foreach (var nextButton in machine.Buttons)
+                {
+                    if (nextButton == pressedButton) continue; //dont press the same twice in a row
+                    FlipSwitches([.. state], flipCount, nextButton);
+                }
+            }
+        }));
+    }
+    record Machine(bool[] LightGoalState, Button[] Buttons);
+    record Button(int[] Modifiers);
+}
---
+++ 2025/10/Task2.cs
@@ -0,0 +1,73 @@
+using CommonLib;
+using System.Diagnostics;
+using System.Text.RegularExpressions;
+
+public class Task2(string[] input) : BaseTask()
+{
+    //works like a charm if you have a couple of years to run it
+    protected override void Solve()
+    {
+        var machines = input.Select(line =>
+        {
+            var joltageGoalState = Regex.Match(line, @"\{([^{}]*)\}").Groups[1].Value.Split(",").Select(c => int.Parse(c)).ToArray();
+            var buttons = Regex.Matches(line, @"\((\d+(?:\s*,\s*\d+)*)\)").Select(match =>
+            {
+                var split = match.Groups[1].Value.Split(",");
+                return new Button([.. split.Select(s => int.Parse(s))]);
+            }).OrderByDescending(b => b.Modifiers.Sum()).ToArray();
+            return new Machine(joltageGoalState, buttons);
+        }).ToList();
+
+        Console.WriteLine(machines.Sum(machine =>
+        {
+            var stopWatch = new Stopwatch();
+            stopWatch.Start();
+            int? bestFlipCount = null;
+            var dynamicFailOutValue = 1;
+            while (bestFlipCount == null)
+            {
+                FlipSwitches([.. Enumerable.Range(0, machine.JoltageGoalState.Length).Select(i => 0)], 0);
+                Console.WriteLine($"DynamicFailout = {dynamicFailOutValue}, Time = {stopWatch.ElapsedMilliseconds}ms");
+                dynamicFailOutValue++;
+            }
+            Console.WriteLine($"{machines.IndexOf(machine) + 1}/{machines.Count}, Result = {bestFlipCount.Value}, DynamicFailOut= {dynamicFailOutValue-1}");
+            return bestFlipCount;
+
+            void FlipSwitches(int[] state, int flipCount, Button? pressedButton = null)
+            {
+                if (pressedButton != null)
+                {
+                    foreach (var modifier in pressedButton.Modifiers)
+                    {
+                        state[modifier]++;
+                    }
+                    flipCount++;
+                }
+                if (flipCount > dynamicFailOutValue) return;
+
+                if (bestFlipCount != null && flipCount > bestFlipCount) return;
+                if (state.SequenceEqual(machine.JoltageGoalState))
+                {
+                    if (bestFlipCount == null || flipCount < bestFlipCount) bestFlipCount = flipCount;
+                    return;
+                }
+
+                foreach (var nextButton in machine.Buttons)
+                {
+                    var wouldOverflow = false;
+                    foreach (var modifier in nextButton.Modifiers)
+                    {
+                        if (state[modifier]+1 > machine.JoltageGoalState[modifier])
+                        {
+                            wouldOverflow = true; break;
+                        }
+                    }
+                    if (wouldOverflow) continue;
+                    FlipSwitches([.. state], flipCount, nextButton);
+                }
+            }
+        }));
+    }
+    record Machine(int[] JoltageGoalState, Button[] Buttons);
+    record Button(int[] Modifiers);
+}
