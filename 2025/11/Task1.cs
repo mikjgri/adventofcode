067dbd5399b812ae3bc2dfd72009992e6ac2a5c0
Mikael Grimstad <mikael.grimstad@gmail.com>
2025-12-11 21:18:33 +0100

    1ms

--- 2025/11/Task1.cs
+++ 2025/11/Task1.cs
@@ -4,45 +4,29 @@ public class Task1(string[] input) : BaseTask()
 {
     protected override object Solve()
     {
-        var endNode = new Node("out");
-
-        //build node structure
-        var childrenTempDict = new Dictionary<string,List<string>>();
         var nodes = input.Select(line =>
         {
             var split = line.Split(": ");
-            childrenTempDict.Add(split[0],[.. split[1].Split(" ").Select(str => str)]);
-            return new Node(split[0]);
-        }).ToList();
-        nodes.Add(endNode);
-        foreach(var node in nodes)
-        {
-            if (!childrenTempDict.TryGetValue(node.Id, out var children)) continue;
-            var childNodes = children.Select(child => nodes.FirstOrDefault(n => n.Id == child));
-            node.Children = [.. node.Children.Union(childNodes ?? [])!];
-        }
+            return (split[0], split[1].Split(" ").Select(str => str).ToArray());
+        }).ToDictionary(a => a.Item1, a => a.Item2);
+        nodes.Add("out", []);
 
         var result = 0;
 
-        RunToOut(nodes.First(node => node.Id == "you"));
-
+        RunToOut("you");
         return result;
 
-        void RunToOut(Node node)
+        void RunToOut(string nodeId)
         {
-            if (node == endNode)
+            if (nodeId == "out")
             {
                 result++;
                 return;
             }
-            foreach (var child in node.Children)
+            foreach (var child in nodes[nodeId])
             {
                 RunToOut(child);
             }
         }
-    }
-    record Node(string Id)
-    {
-        public Node[] Children = [];
     }
 }
