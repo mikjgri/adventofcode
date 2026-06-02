045a9dfc38188af2117ddc5c45e345638271ff3b
GRIMSTAD Mikael <mikael.grimstad@soprasteria.com>
2026-06-02 14:03:44 +0200

    2021-03

---
+++ 2021/03/Task1.cs
@@ -0,0 +1,16 @@
+using CommonLib.Solvers;
+
+public class Task1(string[] input) : BaseTask()
+{
+    protected override object Solve()
+    {
+        var gammaRate = "";
+        for (var i = 0; i < input[0].Length; i++)
+        {
+            var bits = input.Select(line => line[i]);
+            gammaRate += bits.Count(b => b == '1') > input.Length / 2 ? "1" : "0";
+        }
+        var epsilonRate = string.Concat(gammaRate.Select(bit => bit == '1' ? '0' : '1'));
+        return Convert.ToInt32(gammaRate, 2) * Convert.ToInt32(epsilonRate, 2);
+    }
+}
---
+++ 2021/03/Task2.cs
@@ -0,0 +1,27 @@
+using CommonLib.Solvers;
+
+public class Task2(string[] input) : BaseTask()
+{
+    protected override object Solve()
+    {
+        var oxygenGeneratorRating = FilterDownToSingle(input, true);
+        var co2scrubberRating = FilterDownToSingle(input, false);
+        return Convert.ToInt32(oxygenGeneratorRating, 2) * Convert.ToInt32(co2scrubberRating, 2);
+    }
+
+    private string FilterDownToSingle(string[] numbers, bool keepHighest, int index = 0)
+    {
+        if (numbers.Length == 1) return numbers[0];
+        IEnumerable<string> filteredNumbers;
+        var onesMatch = numbers.Where(n => n[index] == '1');
+        var zerosMatch = numbers.Where(n => n[index] == '0');
+        if (onesMatch.Count() == zerosMatch.Count())
+        {
+            filteredNumbers = keepHighest ? onesMatch : zerosMatch;
+        }
+
+        filteredNumbers = (onesMatch.Count() < zerosMatch.Count()) == keepHighest ? onesMatch : zerosMatch;
+
+        return FilterDownToSingle([.. filteredNumbers], keepHighest, index + 1);
+    }
+}
