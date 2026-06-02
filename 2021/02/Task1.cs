13e2109eef0e54abcbd2eee5570fd3d41cdf94f9
GRIMSTAD Mikael <mikael.grimstad@soprasteria.com>
2026-06-02 12:54:55 +0200

    2021-02

---
+++ 2021/02/Task1.cs
@@ -0,0 +1,28 @@
+using CommonLib.Solvers;
+
+public class Task1(string[] input) : BaseTask()
+{
+    protected override object Solve()
+    {
+        var x = 0;
+        var z = 0;
+        foreach (var line in input)
+        {
+            var s = line.Split(" ");
+            var direction = s[0];
+            var movement = int.Parse(s[1]);
+            if (direction == "forward")
+            {
+                x += movement;
+                continue;
+            }
+            if (direction == "down")
+            {
+                z += movement;
+                continue;
+            }
+            z -= movement;
+        }
+        return x * z;
+    }
+}
---
+++ 2021/02/Task2.cs
@@ -0,0 +1,31 @@
+using CommonLib;
+using CommonLib.Solvers;
+
+public class Task2(string[] input) : BaseTask()
+{
+    protected override object Solve()
+    {
+        var x = 0;
+        var z = 0;
+        var aim = 0;
+        foreach (var line in input)
+        {
+            var s = line.Split(" ");
+            var direction = s[0];
+            var value = int.Parse(s[1]);
+            if (direction == "forward")
+            {
+                x += value;
+                z += aim * value;
+                continue;
+            }
+            if (direction == "down")
+            {
+                aim += value;
+                continue;
+            }
+            aim -= value;
+        }
+        return x * z;
+    }
+}
