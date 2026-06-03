6a1fabbac88a94276e0178c30b11fe9195cf9bc5
GRIMSTAD Mikael <mikael.grimstad@soprasteria.com>
2026-06-03 12:08:51 +0200

    2021-07

---
+++ 2021/07/Task1.cs
@@ -0,0 +1,26 @@
+using CommonLib;
+using CommonLib.Solvers;
+
+public class Task1(string[] input) : BaseTask()
+{
+    protected override object Solve()
+    {
+        var crabs = input[0].Split(",").Select(p => int.Parse(p));
+
+        var minPos = crabs.Min();
+        var maxPos = crabs.Max();
+
+        var fuelToPos = new Dictionary<int, int>();
+        for (var i = minPos; i<=maxPos; i++)
+        {
+            var fuelConsumption = 0;
+            foreach (var crab in crabs)
+            {
+                fuelConsumption += int.Abs(i - crab);
+            }
+            fuelToPos.Add(i, fuelConsumption);
+        }
+
+        return fuelToPos.Values.Min();
+    }
+}
---
+++ 2021/07/Task2.cs
@@ -0,0 +1,29 @@
+using CommonLib;
+using CommonLib.Solvers;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+public class Task2(string[] input) : BaseTask()
+{
+    protected override object Solve()
+    {
+        var crabs = input[0].Split(",").Select(p => int.Parse(p));
+
+        var minPos = crabs.Min();
+        var maxPos = crabs.Max();
+
+        var fuelToPos = new Dictionary<int, int>();
+        for (var i = minPos; i <= maxPos; i++)
+        {
+            var fuelConsumption = 0;
+            foreach (var crab in crabs)
+            {
+                var crabFuelCost = int.Abs(i - crab);
+                var triangularNumber = crabFuelCost * (crabFuelCost + 1) / 2;
+                fuelConsumption += triangularNumber;
+            }
+            fuelToPos.Add(i, fuelConsumption);
+        }
+
+        return fuelToPos.Values.Min();
+    }
+}
