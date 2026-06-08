68930b0e5471085027642f3fa89e0c93e0d59610
GRIMSTAD Mikael <mikael.grimstad@soprasteria.com>
2026-06-08 13:02:18 +0200

    2021-09

---
+++ 2021/09/Task1.cs
@@ -0,0 +1,24 @@
+using CommonLib;
+using CommonLib.Solvers;
+
+public class Task1(string[] input) : BaseTask()
+{
+    protected override object Solve()
+    {
+        var grid = input.Select(row => row.Select(col => int.Parse(col.ToString())).ToList()).ToList();
+        var coordinates = GridTools.GenerateCoordinates(grid[0].Count, grid.Count);
+
+        var offsets = GridTools.GetSquare4DirectionOffsets();
+
+        return coordinates.Sum(coord =>
+        {
+            var gVal = grid[coord.y][coord.x];
+            return offsets.All(offset =>
+            {
+                (int x, int y) oCoord = (coord.x + offset.xOff, coord.y + offset.yOff);
+
+                return GridTools.IsInGrid(oCoord, grid) ? grid[oCoord.y][oCoord.x] > gVal : true;
+            }) ? gVal+1 : 0;
+        });
+    }
+}
---
+++ 2021/09/Task2.cs
@@ -0,0 +1,45 @@
+using CommonLib;
+using CommonLib.Solvers;
+
+public class Task2(string[] input) : BaseTask()
+{
+    protected override object Solve()
+    {
+        var grid = input.Select(row => row.Select(col => int.Parse(col.ToString())).ToList()).ToList();
+        var coordinates = GridTools.GenerateCoordinates(grid[0].Count, grid.Count);
+
+        var offsets = GridTools.GetSquare4DirectionOffsets();
+
+        var basins = new Dictionary<(int x, int y), List<(int x, int y)>>();
+        var processedCoords = new List<(int x, int y)>();
+
+        foreach (var coord in coordinates)
+        {
+            var basin = GetBasin(coord);
+            if (basin.Any())
+            {
+                var lowestPoint = FindLowestPoint(basin);
+                basins.TryAdd(lowestPoint, basin);
+            }
+        }
+        var highestBasins = basins.OrderByDescending(basin => basin.Value.Count).Take(3).Select(basin => basin.Value.Count).ToList();
+        return highestBasins[0] * highestBasins[1] * highestBasins[2];
+
+        List<(int x, int y)> GetBasin((int x, int y) pos, List<(int x, int y)>? basin = default)
+        {
+            basin ??= [];
+            if (!GridTools.IsInGrid(pos, grid) || basin.Contains(pos) || processedCoords.Contains(pos)) return basin;
+            processedCoords.Add(pos);
+            var gVal = grid[pos.y][pos.x];
+            if (gVal == 9) return basin;
+            basin.Add(pos);
+            var subBasins = offsets.Select(oCoord => GetBasin((pos.x + oCoord.xOff, pos.y + oCoord.yOff), [.. basin]));
+            var a = subBasins.SelectMany(basin => basin).Distinct().ToList();
+            return a;
+        }
+        (int x, int y) FindLowestPoint(List<(int x, int y)> basin)
+        {
+            return basin.OrderBy(bCoord => grid[bCoord.y][bCoord.x]).First();
+        }
+    }
+}
