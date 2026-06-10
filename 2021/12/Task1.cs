437779793d15206940048bada6f373ecc2b12099
GRIMSTAD Mikael <mikael.grimstad@soprasteria.com>
2026-06-10 14:31:59 +0200

    2021-12

---
+++ 2021/12/Task1.cs
@@ -0,0 +1,30 @@
+using CommonLib;
+using CommonLib.Solvers;
+
+public class Task1(string[] input) : BaseTask()
+{
+    protected override object Solve()
+    {
+        var tree = new Dictionary<string, HashSet<string>>();
+
+        foreach (var line in input)
+        {
+            var s = line.Split("-");
+            tree.TryAdd(s[0], []);
+            tree.TryAdd(s[1], []);
+            tree[s[0]].Add(s[1]);
+            tree[s[1]].Add(s[0]);
+        }
+
+        int WalkTheWalk(string currentCave, List<string> walkedPath)
+        {
+            if (currentCave == "end") return 1;
+            if (char.IsLower(currentCave, 0) && walkedPath.Contains(currentCave)) return 0;
+            walkedPath.Add(currentCave);
+            var connectedNodes = tree[currentCave];
+            return connectedNodes.Sum(cn => WalkTheWalk(cn, [.. walkedPath]));
+        }
+
+        return WalkTheWalk("start", []);
+    }
+}
---
+++ 2021/12/Task2.cs
@@ -0,0 +1,38 @@
+using CommonLib;
+using CommonLib.Solvers;
+
+public class Task2(string[] input) : BaseTask()
+{
+    protected override object Solve()
+    {
+        var tree = new Dictionary<string, HashSet<string>>();
+
+        foreach (var line in input)
+        {
+            var s = line.Split("-");
+            tree.TryAdd(s[0], []);
+            tree.TryAdd(s[1], []);
+            tree[s[0]].Add(s[1]);
+            tree[s[1]].Add(s[0]);
+        }
+
+        int WalkTheWalk(string currentCave, List<string> walkedPath, bool doubleVisitUsed = false)
+        {
+            if (currentCave == "end") return 1;
+            if (walkedPath.Count != 0 && currentCave == "start") return 0;
+            if (char.IsLower(currentCave, 0) && walkedPath.Contains(currentCave))
+            {
+                if (doubleVisitUsed)
+                {
+                    return 0;
+                }
+                doubleVisitUsed = true;
+            }
+            walkedPath.Add(currentCave);
+            var connectedNodes = tree[currentCave];
+            return connectedNodes.Sum(cn => WalkTheWalk(cn, [.. walkedPath], doubleVisitUsed));
+        }
+
+        return WalkTheWalk("start", []);
+    }
+}
